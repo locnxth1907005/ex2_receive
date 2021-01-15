80adbb83259c4cf0c24db60888940136c8ae1b17
locnxth1907005 <locnxth1907005@fpt.edu.vn>
2021-01-15 21:10:28 +0700

    first commit

---
+++ URLRecive/MyDBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URLRecive
+{
+    class MyDBContext : DbContext
+    {
+    public MyDBContext(): base("MyConnectionString"){
+            }
+
+        public DbSet<Article> Article { get; set; }
+    }
+}
+   
+    
+
